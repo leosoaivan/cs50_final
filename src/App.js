59f4c7747f965acbf6a9f36b2bb54c6b1684cc45
Leonard Soai-Van <leo.soaivan@gmail.com>
2021-11-05 21:22:14 -0400

    Add react95

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
+import GlobalStyles from './styles/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import original from "react95/dist/themes/original";
 import { BrowserRouter as Router, Route, Switch, useHistory } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./util/firebaseAuth";
 import UserContext from './context/UserContext';
-import Login from './components/authForms/Login'
-import Register from './components/authForms/Register'
-import Reset from './components/authForms/Reset'
-import Dashboard from './components/pages/Dashboard'
+import Login from './components/authForms/Login';
+import Register from './components/authForms/Register';
+import Reset from './components/authForms/Reset';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   const [user] = useAuthState(auth);
@@ -19,22 +22,25 @@ function App() {
   }, [user, history]);
 
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          { user ? (
-            <UserContext.Provider value={user}>
-              <Route exact path="/" component={Dashboard} />
-            </UserContext.Provider>
-          ) : (
-            <React.Fragment>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/reset" component={Reset} />
-            </React.Fragment>
-          )}
-        </Switch>
-      </Router>
+    <div>
+      <GlobalStyles />
+      <ThemeProvider theme={original}>
+        <Router>
+          <Switch>
+            { user ? (
+              <UserContext.Provider value={user}>
+                <Route exact path="/" component={Dashboard} />
+              </UserContext.Provider>
+            ) : (
+              <React.Fragment>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/reset" component={Reset} />
+              </React.Fragment>
+            )}
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
