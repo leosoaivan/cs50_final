1dfe4b50970106b1e7d41384e1cf6a295ccbf1ab
Leonard Soai-Van <leo.soaivan@gmail.com>
2021-11-07 15:19:06 -0500

    Create Dropdown menu

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,11 +1,34 @@
 import React, { useCallback, useEffect, useState, useContext } from "react";
 import { useHistory } from "react-router";
+import styled from 'styled-components';
+import {
+  Button,
+  Divider,
+  List,
+  ListItem,
+  Toolbar,
+  Window,
+  WindowContent,
+  WindowHeader,
+} from 'react95';
+
 import UserContext from '../../context/UserContext';
 import { auth } from "../../util/firebaseAuth";
 import { getUserProfile } from '../../util/firebaseFirestore';
 
+const Root = styled.div`
+  #toolbar button:nth-of-type(1) {
+    margin-right: 8px;
+  }
+`
+
+const Dropdown = styled.div`
+  position: relative;
+`
+
 function Dashboard() {
   const user = useContext(UserContext)
+  const [open, setOpen] = useState(false);
   const [profile, setProfile] = useState(undefined);
   const history = useHistory();
 
@@ -31,16 +54,54 @@ function Dashboard() {
   }
 
   return (
-    <div className="dashboard">
-      <div className="dashboard__container">
-        Logged in as
-        <div>{profile?.name}</div>
-        <div>{profile?.email}</div>
-        <button className="dashboard__btn" onClick={handleLogout}>
-          Logout
-        </button>
-      </div>
-    </div>
+    <Root>
+      <Window>
+        <WindowHeader className='window-header'>
+          <span>magic8ball.exe</span>
+        </WindowHeader>
+        <Toolbar id="toolbar">
+          <Dropdown>
+            <Button
+              variant='menu'
+              size='sm'
+              onClick={() => setOpen(!open)}
+              active={open}
+            >
+              Go To
+            </Button>
+            {open && (
+              <List
+                style={{
+                  position: 'absolute',
+                  top: '100%',
+                  zIndex: '9999'
+                }}
+                open={open}
+                onClick={() => setOpen(false)}
+              >
+                <ListItem size='sm'>Dashboard</ListItem>
+                <Divider />
+                <ListItem size='sm'>Forum</ListItem>
+              </List>
+            )}
+          </Dropdown>
+          <Button
+            variant='menu'
+            size='sm'
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </Toolbar>
+        <WindowContent>
+          <div>
+            Logged in as
+            <div>{profile?.name}</div>
+            <div>{profile?.email}</div>
+          </div>
+        </WindowContent>
+      </Window>
+    </Root>
   );
 }
 export default Dashboard;
